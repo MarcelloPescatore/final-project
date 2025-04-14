95110f7a2dfb194ab44bf2e193098bdbff28658c
MarcelloPescatore <pescatoremarcello@gmail.com>
2025-04-14 18:46:13 +0200

    fix structure and styles

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logo from '../assets/images/logo.png';
 
 export default function AppHeader() {
 
@@ -6,19 +7,21 @@ export default function AppHeader() {
     return (
         <header className='d-flex justify-content-between px-sm-5 py-4 align-items-center'>
             <div>
-                <span className='fs-4' >GameLand</span >
+                <a href="/">
+                    <img src={logo} alt="logo" />
+                </a>
             </div>
 
             <div>
                 <ul className='d-flex gap-4 align-items-center m-0 p-0'>
                     <li>
-                        <a className='' href="/videogiochi">Giochi</a>
+                        <a href="/videogiochi">Giochi</a>
                     </li>
                     <li>
-                        <a className='' href="/novità">Novità</a>
+                        <a href="/novità">Novità</a>
                     </li>
                     <li>
-                        <a className='' href="/contatti">Contatti</a>
+                        <a href="/contatti">Contatti</a>
                     </li>
                 </ul>
             </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,86 @@
-export default function HomePage(){
+import { useContext, useMemo, useState, useEffect } from 'react'
+import DataContext from '../context/DataContext'
+
+export default function HomePage() {
+    const { data, loading, error } = useContext(DataContext);
+
+    /* funzione per votazione in stelle */
+    const renderStars = (vote) => {
+        const fromeOneToFive = Math.ceil(vote * 0.5)
+        const stars = []
+
+        for (let i = 1; i <= 5; i++) {
+            if (i <= fromeOneToFive) {
+                stars.push(<i key={i} className="bi bi-star-fill text-warning me-2"></i>);
+            } else {
+                stars.push(<i key={i} className="bi bi-star text-warning me-2"></i>);
+            }
+        }
+
+        return stars
+    }
+
 
     return (
-        <div className="container my-3">
-            <h1> Homepage </h1>
+        <div id="homepage">
+            <div className="image-wrapper">
+                <img src="/sfondo.jpg" alt="sfondo" />
+                <div className="fade-bottom"></div>
+            </div>
+            <div className="jumbotron d-flex align-items-center justify-content-center">
+                <h2>Benvenuto su GameLand</h2>
+            </div>
+
+            <div className="container position-relative z-2" id='catalogo'>
+                {loading ? (
+                    <div className="d-flex justify-content-center align-items-center vh-100">
+                        <div className="spinner-border text-primary" role="status">
+                            <span className="visually-hidden">Caricamento...</span>
+                        </div>
+                    </div>
+                ) : error ? (
+                    <div className="d-flex justify-content-center align-items-center vh-100">
+                        <div className="alert alert-danger text-center fs-5 px-5 py-4" role="alert">
+                            Ci scusiamo per il disagio, si è verificato un errore. <br />
+                            Ricaricare la pagina, grazie.
+                        </div>
+                    </div>
+                ) : (
+
+                    <div className='d-flex flex-column my-5'>
+
+                        <h3 className='cl_text'>Catalogo Videogiochi</h3>
+
+                        {/* videogames list */}
+                        <div className='d-flex flex-column mt-4 col-12 '>
+                            {/* card */}
+                            <div className='d-flex gap-5' >
+                                {
+                                    data.slice(0, 10).map((videogioco) => (
+                                        <div className="card col-5 card-videogioco rounded" style={{ width: '19rem' }} key={videogioco.id} >
+                                            <img src={videogioco.copertinaUrl} alt="..." className='rounded' />
+                                            <div className="card-body d-flex flex-column  justify-content-end py-4">
+                                                <div>
+                                                    <h5 className="card-title">{videogioco.titolo}</h5>
+                                                    <span>{renderStars(videogioco.voto)}</span>
+                                                </div>
+                                                <a href="#" className="btn btn-primary mt-3 fw-bold">Scopri di più</a>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                            {/* mostra di più */}
+                            <button className='btn align-self-center btn-outline-primary col-2 mt-5'>
+                                <a href="/videogiochi" className='fw-bold'>
+                                    Vedi tutti
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
